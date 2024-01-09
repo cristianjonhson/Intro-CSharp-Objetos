ba4c407e90dfb5036a6736a322f41e3400b9c2b7
Cristian Jonhson Alvarez <cjonhson@actis.cl>
2024-01-08 23:15:59 -0300

    Creacion Objetos

---
+++ Creacion-Objetos/Creacion-Objetos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Creacion_Objetos
+{
+    class Sale
+    {
+        public decimal Total { get; set; }
+        public decimal Balance { get; set; }
+        public string SaleId { get; set; }
+        public string SaleName { get; set; }
+
+        // Constructor
+        public Sale(decimal total, string saleName)
+        {
+            Total = total;
+            SaleName = saleName;
+        }
+
+        public string GetInfo()
+        {
+            return SaleName;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale sale = new Sale(500, "Iphone x");
+
+            // Puedes acceder a las propiedades y métodos de Sale
+            Console.WriteLine($"Total: {sale.Total}, SaleName: {sale.GetInfo()}");
+            // Esperar la entrada del usuario antes de cerrar la aplicación
+            Console.WriteLine("Presiona Enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
